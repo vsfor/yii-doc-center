c813bbf7265758f1012fcf685e82383c6a709bd7
wj922 <wj922@qq.com>
2016-09-23 18:24:52 +0800

    reset template content and styles

--- _protected/components/TemplateLib.php
+++ _protected/components/TemplateLib.php
@@ -21,27 +21,27 @@ class TemplateLib extends Component
         switch ($id) {
             case 'api':
                 $content = '
-
-
-**简要描述：** 
+**简要描述：**
 
 - 用户注册接口
 
-**请求URL：** 
-- ` http://xx.com/api/user/register `
-  
-**请求方式：**
-- POST 
+**请求URL：**
 
-**参数：** 
+- ` http://xx.com/api/user/register `
+
+**请求方式：**
+
+- POST
+
+**参数：**
 
 |参数名|必选|类型|说明|
-|:----    |:---|:----- |-----   |
-|username |是  |string |用户名   |
-|password |是  |string | 密码    |
-|nickname     |否  |string | 昵称    |
+|:--|:--|:--|:--|
+|username|是|string|用户名|
+|password|是|string|密码|
+|nickname|否|string|昵称|
 
- **返回示例**
+**返回示例**
 
 ``` 
   {
@@ -59,13 +59,13 @@ class TemplateLib extends Component
   }
 ```
 
- **返回参数说明** 
+**返回参数说明**
 
 |参数名|类型|说明|
-|:-----  |:-----|-----                           |
-|groupid |int   |用户组id，1：超级管理员；2：普通用户  |
+|:--|:--|:--|
+|groupid|int|用户组id,1超级管理员,2普通用户|
 
- **备注** 
+**备注**
 
 - 更多返回错误代码请看首页的错误代码描述
 
@@ -73,17 +73,15 @@ class TemplateLib extends Component
                 '; break;
             case 'table':
                 $content = '
-
-
 -  用户表，储存用户信息
 
 |字段|类型|空|默认|注释|
-|:----    |:-------    |:--- |-- -|------      |
-|user_id	  |int(10)     |否	|	 |	           |
+|:--|:--|:--|:--|:--|
+|user_id  |int(10)     |否	|	 |	 用户ID  |
 |username |varchar(20) |否	|    |	 用户名	|
-|password |varchar(50) |否   |    |	 密码		 |
-|nickname     |varchar(15) |是   |    |    昵称     |
-|reg_time |int(11)     |否   | 0  |   注册时间  |
+|password |varchar(50) |否  |    |	 密码    |
+|nickname |varchar(15) |是  |    |   昵称   |
+|reg_time |int(11)     |否  | 0  |   注册时间 |
 
 - 备注：无
 
