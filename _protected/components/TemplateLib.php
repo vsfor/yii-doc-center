899827a3930461290747a7ba4655d6ae19806f7f
wj922 <wj922@qq.com>
2017-04-18 18:37:51 +0800

    update for doc template

--- _protected/components/TemplateLib.php
+++ _protected/components/TemplateLib.php
@@ -27,7 +27,7 @@ class TemplateLib extends Component
 
 **请求URL：**
 
-- ` http://xx.com/api/user/register `
+- ` api/user/register `
 
 **请求方式：**
 
@@ -35,58 +35,44 @@ class TemplateLib extends Component
 
 **参数：**
 
-|参数名|必选|类型|说明|
+|参数名|必要|类型|说明|
 |:--|:--|:--|:--|
-|username|是|string|用户名|
-|password|是|string|密码|
-|nickname|否|string|昵称|
+| username | 是 | string | 用户名 |
+| password | 是 | string | 密码 |
+| nickname | 否 | string | 昵称 |
 
-**返回示例**
-
-``` 
-  {
-    "resultCode": 1000,
-    "resultData": {
-      "user_id": "1",
-      "username": "12154545",
-      "nickname": "吴系挂",
-      "group_id": 2 ,
-      "reg_time": "1436864169",
-      "last_login_time": "0",
-    },
-    "dataZipped": 0,
-    //...
-  }
-```
 
 **返回参数说明**
 
 |参数名|类型|说明|
 |:--|:--|:--|
-|groupid|int|用户组id,1超级管理员,2普通用户|
+| groupid | int | 用户组id,1超级管理员,2普通用户 |
 
 **备注**
 
 - 更多返回错误代码请看首页的错误代码描述
 
 
-                '; break;
+'; break;
             case 'table':
                 $content = '
--  用户表，储存用户信息
+####用户主表 ll_user
+
+-  储存用户登录及状态信息
 
 |字段|类型|空|默认|注释|
 |:--|:--|:--|:--|:--|
-|user_id  |int(10)     |否	|	 |	 用户ID  |
-|username |varchar(20) |否	|    |	 用户名	|
-|password |varchar(50) |否  |    |	 密码    |
-|nickname |varchar(15) |是  |    |   昵称   |
-|reg_time |int(11)     |否  | 0  |   注册时间 |
+| id | int(11) | 否 | - | 用户ID|
+| mobile | bigint(11) | 否 | 0 | 手机号 |
+| password |char(32)| 否 |  | 密码 |
+| status | int(11) | | 1 | 状态 |
+| created_at | int(11) | | 0 | 创建时间 |
+| updated_at | int(11) | | 0 | 更新时间 |
 
 - 备注：无
-
-
-                '; break;
+ · 唯一索引：手机号
+ · 手机号为 11 位，注册时需要通过正则匹配
+'; break;
         }
         return $content;
     }
